c35cc6ca1214fc4b223a6d1f85e31516f005f57c
woterr <shreyasv320@gmail.com1>
2025-09-29 19:00:27 +0530

    h

--- no.threads.py
+++ no.threads.py
@@ -11,9 +11,8 @@ for filename in os.listdir(FOLDER):
     with open(filepath, "r", encoding="utf-8") as f:
         post = json.load(f)
         comments_list = post.get("comments", [])
-        with open("comment_ids.txt", "a") as f3:
-            for comment in comments_list:
-                comment_ids.append(comment["id"])
+        for comment in comments_list:
+            comment_ids.append(comment["id"])
         total_threads += len(comments_list)
 
 print(total_threads)
